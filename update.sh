70ed235f0ea880c665825066df690148007895b4
Richie Cahill <richie@tmmworkshop.com>
2022-01-11 10:51:00 -0500

    added favicon twiter and linkedin to index.html fixed spelling mistake on resume added auto restart to update.sh make the sight more moble frendly

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-V=0.4.1
+V=0.5.1
 docker build -t themadmaker2/website:$V .
 docker stop Website
 docker rm Website
-docker run -d --name=Website \-p 5000:5000 themadmaker2/website:$V
+docker run -d --restart unless-stopped --name=Website -p 5000:5000 themadmaker2/website:$V
