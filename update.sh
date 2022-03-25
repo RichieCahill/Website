bf565162aababdeca4255d586147b06a3b1dd994
Richie Cahill <richie@tmmworkshop.com>
2022-03-25 01:28:10 -0400

    setup docker compose

--- update.sh
+++ update.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-V=0.5.1
-docker build -t themadmaker2/website:$V .
-docker stop Website
-docker rm Website
-docker run -d --restart unless-stopped --name=Website -p 5000:5000 themadmaker2/website:$V
+docker-compose -f ./test.yml up -d --build --force-recreate
